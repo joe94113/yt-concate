9a3e279223d1230fa8fff03cdad1562293b4fecd
joe <joe94113@gmail.com>
2020-11-03 20:50:44 +0800

    Successfully got video urls from channel and add requirements.txt

---
+++ yt_concate/main.py
@@ -0,0 +1,34 @@
+import urllib.request
+import json
+from yt_concate.settings import API_KEY
+
+
+def get_all_video_in_channel(channel_id):
+    api_key = 'AIzaSyBefVnQsGyS4CpWRUvWk_01VqkG53iEazU'
+
+    base_video_url = 'https://www.youtube.com/watch?v='
+    base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
+
+    first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(api_key,
+                                                                                                        channel_id)
+
+    video_links = []
+    url = first_url
+    while True:
+        inp = urllib.request.urlopen(url)
+        resp = json.load(inp)
+
+        for i in resp['items']:
+            if i['id']['kind'] == "youtube#video":
+                video_links.append(base_video_url + i['id']['videoId'])
+
+        try:
+            next_page_token = resp['nextPageToken']
+            url = first_url + '&pageToken={}'.format(next_page_token)
+        except KeyError:
+            break
+    return video_links
+
+
+video_list = get_all_video_in_channel(API_KEY)
+print(video_list)
---
+++ yt_concate/settings.py
@@ -0,0 +1,4 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+API_KEY = os.getenv("API_KEY")

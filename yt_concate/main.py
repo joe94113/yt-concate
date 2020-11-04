2734082ef0631a617eeaf30c40875e3f36c210f3
joe <joe94113@gmail.com>
2020-11-04 15:09:55 +0800

    Refactor code into pipeline design pattern

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -1,35 +1,22 @@
-import urllib.request
-import json
-from yt_concate.settings import API_KEY
+from yt_concate.pipeline.Steps.get_video_list import GetVideoList
+from yt_concate.pipeline.Steps.step import StepException
+from yt_concate.pipeline.pipeline import Pipeline
 
 CHANNEL_ID = "UCKSVUHI9rbbkXhvAXK-2uxA"
 
 
-def get_all_video_in_channel(channel_id):
+def main():
+    inputs = {
+        'channel_id': CHANNEL_ID
+    }
 
-    base_video_url = 'https://www.youtube.com/watch?v='
-    base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
+    steps = [
+        GetVideoList(),
+    ]
 
-    first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(API_KEY,
-                                                                                                        channel_id)
-
-    video_links = []
-    url = first_url
-    while True:
-        inp = urllib.request.urlopen(url)
-        resp = json.load(inp)
-
-        for i in resp['items']:
-            if i['id']['kind'] == "youtube#video":
-                video_links.append(base_video_url + i['id']['videoId'])
-
-        try:
-            next_page_token = resp['nextPageToken']
-            url = first_url + '&pageToken={}'.format(next_page_token)
-        except KeyError:
-            break
-    return video_links
+    p = Pipeline(steps)
+    p.run(inputs)
 
 
-video_list = get_all_video_in_channel(CHANNEL_ID)
-print(video_list)
+if __name__ == "__main__":
+    main()

95c40683b9741289e0e5633a177cb99729c8ed5c
joe <joe94113@gmail.com>
2020-11-06 15:23:05 +0800

    Successfully rea cations into data structure and fixed Step class

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -2,6 +2,7 @@ from yt_concate.pipeline.Steps.get_video_list import GetVideoList
 from yt_concate.pipeline.Steps.download_captions import DownloadCaption
 from yt_concate.pipeline.Steps.preflight import Preflight
 from yt_concate.pipeline.Steps.postflight import Postflight
+from yt_concate.pipeline.Steps.read_caption import ReadCaption
 from yt_concate.pipeline.Steps.step import StepException
 from yt_concate.pipeline.pipeline import Pipeline
 from yt_concate.utils import Utils
@@ -18,12 +19,13 @@ def main():
         Preflight(),
         GetVideoList(),
         DownloadCaption(),
+        ReadCaption(),
         Postflight(),
     ]
 
     utils = Utils()
     p = Pipeline(steps)
-    p.run(inputs, Utils())
+    p.run(inputs, utils)
 
 
 if __name__ == "__main__":
